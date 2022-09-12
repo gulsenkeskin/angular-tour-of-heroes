584c76a9b349547eafb9588185be8e9bdd013685
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-12 20:32:05 +0300

    upd heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes', //bileşenin seçicisi
   templateUrl: './heroes.component.html', //şablon dosya konumu
   styleUrls: ['./heroes.component.css'] //bileşene özel css stil konumu 
 })
 export class HeroesComponent implements OnInit {
-  hero="Windstorm";
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
