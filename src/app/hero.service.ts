66fe594ceb92d9423a686de2f88ea0c779008646
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-13 13:49:02 +0300

    Update hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,30 +3,44 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   // getHeroes():Hero[]{
   //   return HEROES;
   // }
 
-  //asynchronous signature.
-  getHeroes(): Observable<Hero[]> {
 
-    //of(HEROES), sahte kahramanlar dizisi olan tek bir değer yayan bir Observable<Hero[]> döndürür.
-    const heroes = of(HEROES);
-
-    //servis içi servis kullanımı:
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-
+  //UYGULAMA MESSAGEsERVİCE İ ÇOK ÇAĞIRDIĞINDAN ONU BİR LOG METHODUNA SARMAK DOĞRU OLUR.
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
+  //http client ile kahramanları listelemek //sahte sunucudan veri listeleme
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+
+  /*
+    //asynchronous signature.
+    getHeroes(): Observable<Hero[]> {
+      //of(HEROES), sahte kahramanlar dizisi olan tek bir değer yayan bir Observable<Hero[]> döndürür.
+      const heroes = of(HEROES);
+  
+      //servis içi servis kullanımı:
+      // this.messageService.add('HeroService: fetched heroes');
+  
+      return heroes;
+    }
+  */
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
