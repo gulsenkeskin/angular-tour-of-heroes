570e9dec9481bdc598f2a7245df1012a4902b87b
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-13 12:01:19 +0300

    added location service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -8,12 +10,13 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  constructor() { }
+  //Location tarayıcıyla etkileşim kurmak için bir angular servisidir. Bu servis önceki view e görü dönmeyi sağlar.
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit(): void {
   }
 
-  @Input() hero?:Hero; //Hero özelliğini harici HeroesComponent tarafından bağlanmaya uygun hale getirmek için @Input dekoratörünü kullanırız
+  @Input() hero?: Hero; //Hero özelliğini harici HeroesComponent tarafından bağlanmaya uygun hale getirmek için @Input dekoratörünü kullanırız
 
 
   //bu component hero özelliği aracılığıyla bir hero nesnesi alır ve onu görüntüler
