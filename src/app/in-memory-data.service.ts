b6ea0a7dd386e06434b73e786ad0b40f3723e5f7
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-13 13:49:05 +0300

    Update in-memory-data.service.ts

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -24,6 +24,6 @@ export class InMemoryDataService {
   }
 
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id));
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
