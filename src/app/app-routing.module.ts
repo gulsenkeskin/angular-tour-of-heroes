e2f6e239360a2f680e3ebd75e870a6d45afbdd40
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-13 10:27:22 +0300

    router-outlet
    kullanımı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
+
+  //Yönlendiriciyi uygulamanın kök düzeyinde yapılandırdığınız için yöntem forRoot() olarak adlandırılır.
+  //forRoot() yöntemi, yönlendirme için gereken servis sağlayıcıları ve yönergeleri sağlar ve geçerli tarayıcı URL'sine dayalı olarak ilk gezinmeyi gerçekleştirir.
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  //Ardından, AppRoutingModule, RouterModule'u uygulama genelinde kullanılabilir olacak şekilde dışa aktarır.
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
